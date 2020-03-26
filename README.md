1a24361afa12cc55823696cab2fb6874805fefff
Paul Hooijenga <paulhooijenga@gmail.com>
2020-03-26 15:03:24 +0100

    Implement setting and clearing status

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # slack-status
+
 Slack Status CLI
+
+## Installation
+
+    go get -u github.com/hackedd/slack-status
+
+## Usage
+
+    $ export SLACK_TOKEN=...
+    $ slack-status set --duration 30m --emoji ':sparkles:' 'Doing a thing'
+    $ slack-status clear
---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/slack-go/slack"
+	"github.com/urfave/cli"
+)
+
+func connect(c *cli.Context) (*slack.Client, error) {
+	verbose := c.GlobalBool("verbose")
+	token := c.GlobalString("token")
+
+	if token == "" {
+		return nil, fmt.Errorf("no token specified")
+	}
+
+	client := slack.New(token, slack.OptionDebug(verbose))
+
+	authTest, err := client.AuthTest()
+	if err != nil {
+		return nil, err
+	}
+
+	if verbose {
+		log.Printf("Connected to %s as %s\n", authTest.URL, authTest.User)
+	}
+	return client, nil
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "slack-status"
+	app.Usage = "Set your Slack status"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "token",
+			EnvVar: "SLACK_TOKEN",
+			Usage:  "Slack authentication token",
+		},
+		cli.BoolFlag{
+			Name:  "verbose",
+			Usage: "Enable Slack API debug logging",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "set",
+			Usage: "Set Slack status",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "text",
+					Usage: "Status text, for example 'Lunch'",
+				},
+				cli.StringFlag{
+					Name:  "emoji",
+					Usage: "Status emoji, for example :sandwich:",
+				},
+				cli.StringFlag{
+					Name:  "duration",
+					Usage: "Automatically clear status after duration has passed, for example 30m.",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				client, err := connect(c)
+				if err != nil {
+					return fmt.Errorf("unable to connect: %v", err)
+				}
+
+				statusText := c.String("text")
+				if statusText == "" && c.NArg() > 0 {
+					statusText = strings.Join(c.Args(), " ")
+				}
+
+				statusEmoji := c.String("emoji")
+
+				var expiration int64 = 0
+				durationArg := c.String("duration")
+				if durationArg != "" {
+					duration, err := time.ParseDuration(durationArg)
+					if err != nil {
+						log.Fatalf("Unable to parse duration: %v", err)
+					}
+					expiration = time.Now().Add(duration).Unix()
+				}
+
+				if c.GlobalBool("verbose") {
+					msg := fmt.Sprintf("Setting status to '%+v' with emoji '%+v'", statusText, statusEmoji)
+					if expiration > 0 {
+						msg += fmt.Sprintf(" until %v", time.Unix(expiration, 0))
+					}
+					log.Println(msg)
+				}
+
+				err = client.SetUserCustomStatus(statusText, statusEmoji, expiration)
+				if err != nil {
+					return fmt.Errorf("unable to set status: %v", err)
+				}
+
+				return nil
+			},
+		},
+		{
+			Name:  "clear",
+			Usage: "Clear Slack status",
+			Action: func(c *cli.Context) error {
+				client, err := connect(c)
+				if err != nil {
+					return fmt.Errorf("unable to connect: %v", err)
+				}
+
+				err = client.SetUserCustomStatus("", "", 0)
+				if err != nil {
+					return fmt.Errorf("unable to set status: %v", err)
+				}
+
+				return nil
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
+}
